37e3abc79a94c359a9e0902d41e4ff6c55d8c8ee
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2024-01-09 22:45:51 +0100

    feat(scanner): Count number of chairs in each room

--- aptscan/scanner.py
+++ aptscan/scanner.py
@@ -26,22 +26,40 @@ import itertools
 from typing import Dict, Iterable, Tuple
 
 
+EMPTY = " "
+CHAIRS = ["W", "P", "S", "C"]
+WALLS = ["+", "-", "|", "/", "\\"]
 ROOM_NAME_OPEN = "("
 ROOM_NAME_CLOSE = ")"
 
 Position = Tuple[int, int]
 
 
+class Room:
+    """Room represents a room in a floor plan"""
+
+    name: str
+    chairs: Dict[str, int]
+
+    def __init__(self, name: str, chairs: Dict[str, int]) -> None:
+        self.name = name
+        self.chairs = chairs
+
+
 class Scanner:
     """Scanner reads a floor plan and counts the number of chairs in each room"""
 
     floor_map: Dict[Position, str]
-    rooms: Dict[Position, str]
+    rooms: Dict[Position, Room]
 
     def __init__(self, floor_plan: str) -> None:
         self.floor_map = {}
         self.rooms = {}
         self._parse(floor_plan)
+        try:
+            self._count_chairs()
+        except KeyError as err:
+            raise ValueError("invalid floor plan: floor plan is malformed") from err
 
     def _parse(self, floor_plan: str) -> None:
         """Parse the floor plan and populate the floor map and rooms"""
@@ -55,14 +73,37 @@ class Scanner:
                             itertools.takewhile(lambda c: c != ROOM_NAME_CLOSE, line_iter)
                         )
                         for i in range(col, col + len(room_name) + 2):
-                            self.floor_map[(row, i)] = " "
-                        self.rooms[(row, col)] = room_name
+                            self.floor_map[(row, i)] = EMPTY
+                        self.rooms[(row, col)] = Room(room_name, {chair: 0 for chair in CHAIRS})
                         col += len(room_name) + 2
                     case c if c == ROOM_NAME_CLOSE:
                         raise ValueError("invalid floor plan: unexpected closing parenthesis")
                     case _:
                         self.floor_map[(row, col)] = char
                         col += 1
+
+    def _count_chairs(self) -> None:
+        """Count the number of chairs in each room storing the result in the room object"""
+        for room_pos, room in self.rooms.items():
+            next_squares = set([room_pos])
+            seen = set([room_pos])
+            while next_squares:
+                square = next_squares.pop()
+                if self.floor_map[square] in CHAIRS:
+                    room.chairs[self.floor_map[square]] += 1
+                elif self.floor_map[square] is not EMPTY:
+                    raise ValueError(f"invalid floor plan: unexpected character at {square}")
+                for neighbor in self._get_neighbors(square):
+                    if neighbor not in seen:
+                        next_squares.add(neighbor)
+                        seen.add(neighbor)
+
+    def _get_neighbors(self, square: Position) -> Iterable[Position]:
+        """Return the neighbors of a square"""
+        row, col = square
+        for neighbor in ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
+            if self.floor_map.get(neighbor, None) not in WALLS:
+                yield neighbor
 
     def get_floor_map_repr(self) -> str:
         """Return a string representation of the floor map"""
--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -13,31 +13,47 @@ import pytest
 from aptscan import scanner
 
 
-def test_scanner_can_parse_one_room():
-    floor_plan = """+--------+
+@pytest.fixture(name="floor_plan")
+def floor_plan_fixture():
+    return """+--------+
 |(room a)|
 |       C|
 |       C|
 |       C|
 +--------+
 """
-    s1 = scanner.Scanner(floor_plan)
-    assert s1.get_floor_map_repr() == floor_plan.replace("(room a)", "        ")
-    assert s1.rooms == {(1, 1): "room a"}
 
 
-def test_scanner_can_parse_multiple_rooms():
-    floor_plan = """+--------+--------+
+@pytest.fixture(name="floor_plan_multi")
+def floor_plan_multi_fixture():
+    return """+--------+--------+
 |(room a)|(room b)|
 |       C|       C|
 |       C|       C|
 |       C|       C|
 +--------+--------+
 """
+
+
+def test_scanner_can_parse_one_room(floor_plan):
     s1 = scanner.Scanner(floor_plan)
-    expected_repr = floor_plan.replace("(room a)", "        ").replace("(room b)", "        ")
+    assert s1.get_floor_map_repr() == floor_plan.replace("(room a)", "        ")
+    assert len(s1.rooms) == 1
+    assert (1, 1) in s1.rooms
+    assert s1.rooms[(1, 1)].name == "room a"
+
+
+def test_scanner_can_parse_multiple_rooms(floor_plan_multi):
+    s1 = scanner.Scanner(floor_plan_multi)
+    expected_repr = floor_plan_multi.replace("(room a)", "        ").replace(
+        "(room b)", "        "
+    )
     assert s1.get_floor_map_repr() == expected_repr
-    assert s1.rooms == {(1, 1): "room a", (1, 10): "room b"}
+    assert len(s1.rooms) == 2
+    assert (1, 1) in s1.rooms
+    assert s1.rooms[(1, 1)].name == "room a"
+    assert (1, 10) in s1.rooms
+    assert s1.rooms[(1, 10)].name == "room b"
 
 
 def test_scanner_fails_to_parse_a_floor_plan_with_unexpected_closing_parenthesis():
@@ -51,3 +67,101 @@ def test_scanner_fails_to_parse_a_floor_plan_with_unexpected_closing_parenthesis
     with pytest.raises(ValueError) as excinfo:
         scanner.Scanner(floor_plan)
     assert str(excinfo.value) == "invalid floor plan: unexpected closing parenthesis"
+
+
+def test_scanner_fails_to_parse_a_floor_plan_with_a_room_with_an_unclosed_name():
+    floor_plan = """+--------+
+|(room a |
+|       C|
+|       C|
+|       C|
++--------+
+"""
+    with pytest.raises(ValueError) as excinfo:
+        scanner.Scanner(floor_plan)
+    assert str(excinfo.value) == "invalid floor plan: floor plan is malformed"
+
+
+def test_scanner_fails_to_parse_a_floor_plan_with_a_room_with_invalid_object():
+    floor_plan = """+--------+
+|(room a)|
+|       C|
+|  X    C|
+|       C|
++--------+
+"""
+    with pytest.raises(ValueError) as excinfo:
+        scanner.Scanner(floor_plan)
+    assert str(excinfo.value) == "invalid floor plan: unexpected character at (3, 3)"
+
+
+@pytest.mark.parametrize(
+    "floor_plan",
+    [
+        """+--------+
+|(room a)|
+|       C|
+        C|
+|       C|
++--------+
+""",
+        """+---- ---+
+|(room a)|
+|       C|
+|       C|
+|       C|
++--------+
+""",
+        """+--------+
+|(room a)|
+|       C|
+|       C|
+|       C|
++- ------+
+""",
+    ],
+)
+def test_scanner_fails_to_parse_malformed_floor_plans(floor_plan):
+    with pytest.raises(ValueError) as excinfo:
+        scanner.Scanner(floor_plan)
+    assert str(excinfo.value) == "invalid floor plan: floor plan is malformed"
+
+
+def test_scanner_can_count_chairs_in_one_room(floor_plan):
+    s1 = scanner.Scanner(floor_plan)
+    assert s1.rooms[(1, 1)].chairs == {"W": 0, "P": 0, "S": 0, "C": 3}
+
+
+def test_scanner_can_count_chairs_in_multiple_rooms(floor_plan_multi):
+    s1 = scanner.Scanner(floor_plan_multi)
+    assert s1.rooms[(1, 1)].chairs == {"W": 0, "P": 0, "S": 0, "C": 3}
+    assert s1.rooms[(1, 10)].chairs == {"W": 0, "P": 0, "S": 0, "C": 3}
+
+
+def test_scanner_can_count_chairs_in_a_room_with_multiple_chair_types():
+    floor_plan = """+--------+
+|(room a)|
+|       W|
+|     P P|
+|   S S S|
+| C C C C|
++--------+
+"""
+    s1 = scanner.Scanner(floor_plan)
+    assert s1.rooms[(1, 1)].chairs == {"W": 1, "P": 2, "S": 3, "C": 4}
+
+
+def test_scanner_can_count_chairs_in_rooms_with_diagonal_walls():
+    floor_plan = r"""+--------+------------------+      +---+
+|(room a)|                C |      | W |
+|       C|   +----------+   |      |   |
+|       C|   |          |   |      |   |
+|       C+   +------+   |   |      |   |
+|        C\(room b)/    |   |      |   |
+|         C\      /     |   +------+   |
+|          C\    /      | S          P |
++------------+--+       +--------------+
+"""
+    s1 = scanner.Scanner(floor_plan)
+    assert s1.rooms[(1, 1)].chairs == {"W": 0, "P": 0, "S": 0, "C": 6}
+    assert s1.rooms[(5, 11)].chairs == {"W": 1, "P": 1, "S": 1, "C": 1}
